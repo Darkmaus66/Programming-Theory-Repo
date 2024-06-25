31e2f0b35615933aa4ff02bec09cce5a897f2b80
Darkmaus66 <joost.deriks@gmail.com>
2024-06-25 18:40:51 +0200

    playfield
    added playfield with dogs and food

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,23 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class GameController : MonoBehaviour
 {
-    private string playerName;
-    private int dogIndex;
+    [SerializeField] GameObject[] dogs;
+    [SerializeField] GameObject[] foods;
+    [SerializeField] TextMeshProUGUI foodSelected;
+
+
+    public string playerName;
+    public int dogIndex;
+    public string activeFood;
+    public bool isFoodSelected;
+
+    Camera m_Camera;
+
+    private void Awake()
+    {
+        m_Camera = Camera.main;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         playerName = MenuHandler.Instance.playerName;
         dogIndex = MenuHandler.Instance.dogSelectedIndex;
-        Debug.Log("Hello " +  playerName + ", dog " + dogIndex + " is hungry.");
+        dogs[dogIndex].SetActive(true);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        CheckForFoodClick();
+    }
+
+    void CheckForFoodClick()
+    {
+        GameObject objectClicked;
+        Mouse mouse = Mouse.current;
+        if (mouse.leftButton.wasPressedThisFrame)
+        {
+            Vector3 mousePosition = mouse.position.ReadValue();
+            Ray ray = m_Camera.ScreenPointToRay(mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                objectClicked = hit.transform.gameObject;
+                if (objectClicked.CompareTag("Food"))
+                {
+                    FoodSelected(objectClicked.name);
+                }
+                if (objectClicked.CompareTag("Dog") && isFoodSelected)
+                {
+                    DogSelected();
+                }
+            }
+        }
+    }
+
+    public void FoodSelected(string foodName)
+    {
+        activeFood = foodName;
+        foodSelected.text = activeFood;
+        isFoodSelected = true;
+    }
+
+    public void DogSelected()
+    {
+        Debug.Log("Thank you for the " + activeFood);
     }
 }
